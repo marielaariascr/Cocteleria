c747fcdb42bd1ee27d48124aa83c28699d526175
mariassuarez <mariassuarez@LAPTOP-8EB5AR41>
2023-10-03 05:19:44 -0600

    Tarea de cocteleria, Mariela Arias, 10/03/2023

---
+++ src/main/java/com/cocteleria/CocteleriaApplication.java
@@ -0,0 +1,13 @@
+package com.cocteleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocteleriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocteleriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cocteleria/CocteleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocteleria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocteleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
